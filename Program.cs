9adbb9d446b2309b59af56cceae17883f777fb49
juand610 <128150236+juand610@users.noreply.github.com>
2023-03-31 21:12:11 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using DragonsWay.Logic;
+
+Console.WriteLine("*** Dragon's Way ***\n");
+var game = new Game(9, "→→→↓↓↓→→→↓↓↓→→→→→→→→→↓→→→→→→");
+var game2 = new Game(9, "→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→→");
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine(game);
+Console.WriteLine(game2);
+Console.BackgroundColor = ConsoleColor.Black;
+
+
+
+
+//Console.BackgroundColor = ConsoleColor.Blue;
+//Console.ForegroundColor = ConsoleColor.Yellow;
+//var maze = new Game(20, "→→→↓↓↓→→→↓↓↓→→→→→→→→→↓→→→→→→");
+//Console.Write(maze);
+//Console.ForegroundColor = ConsoleColor.White;
+//Console.BackgroundColor = ConsoleColor.Black;
+
